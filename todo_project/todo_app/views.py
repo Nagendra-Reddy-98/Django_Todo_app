012c1d354d27c55c19ed0e290ac3fd735a658df4
nagendra_reddy <nagendrareddy539@gmail.com>
2023-10-22 22:21:21 +0530

    changes updated for adding tasks to todo app

--- todo_project/todo_app/views.py
+++ todo_project/todo_app/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.http import HttpResponse
+from .models import Create_Task
 
 # Create your views here.
+def addTask(request):
+    tasks=request.POST['task']
+    Create_Task.objects.create(task=tasks)
+    return redirect('home')
--- todo_project/todo_project/views.py
+++ todo_project/todo_project/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from todo_app.models import Create_Task
 
 
 def home(request):
     # return HttpResponse("<h1>To-DO Project</h1>")
-    return render(request,'home.html')
+    tasks = Create_Task.objects.filter(is_completed=False).order_by('updated_at')
+    return render(request, 'home.html', context={'tasks': tasks})
