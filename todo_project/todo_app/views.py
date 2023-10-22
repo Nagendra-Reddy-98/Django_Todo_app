599c5534d3f7f5b23fb08f16dfb0d5f885d8460a
nagendra_reddy <nagendrareddy539@gmail.com>
2023-10-22 23:04:02 +0530

    changes updated for adding tasks to todo app

--- todo_project/todo_app/views.py
+++ todo_project/todo_app/views.py
@@ -1,9 +1,28 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.http import HttpResponse
 from .models import Create_Task
 
+
 # Create your views here.
 def addTask(request):
-    tasks=request.POST['task']
+    tasks = request.POST['task']
     Create_Task.objects.create(task=tasks)
     return redirect('home')
+
+
+def mark_as_done(request, pk):
+    task = get_object_or_404(Create_Task, pk=pk)
+    task.is_completed = True
+    task.save()
+    return redirect('home')
+
+def mark_as_undone(request, pk):
+    task = get_object_or_404(Create_Task, pk=pk)
+    task.is_completed = False
+    task.save()
+    return redirect('home')
+
+def delete_task(request,pk):
+    Create_Task.objects.get(pk=pk).delete()
+    return redirect('home')
+
--- todo_project/todo_project/views.py
+++ todo_project/todo_project/views.py
@@ -6,4 +6,5 @@ from todo_app.models import Create_Task
 def home(request):
     # return HttpResponse("<h1>To-DO Project</h1>")
     tasks = Create_Task.objects.filter(is_completed=False).order_by('updated_at')
-    return render(request, 'home.html', context={'tasks': tasks})
+    done_tasks = Create_Task.objects.filter(is_completed=True)
+    return render(request, 'home.html', context={'tasks': tasks, 'done_tasks': done_tasks})
